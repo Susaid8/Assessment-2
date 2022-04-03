a275caa310713f29eaa7ec909aa8a79924090f91
Susaid8 <97367629+Susaid8@users.noreply.github.com>
2022-04-02 23:48:12 -0500

    Update cart.js

--- cart.js
+++ cart.js
@@ -7,13 +7,11 @@
     creating customer objects.  
 */
 
-// will come back to this problem as I didnt finish taking notes on this
 
 //////////////////PROBLEM 1////////////////////
 /*  
     Below is a cart array that has food objects
     inside. 
-
     Write a callback below that uses the reduce
     array method to calculate the sum of all
     the food. 
@@ -36,7 +34,11 @@ const cart = [
 
 //CODE HERE
 
-// const summedPrice = cart.reduce(/* CALLBACK HERE */)
+ // const summedPrice = cart.reduce((cal, food) => {
+ //   return cal + food.price
+ // }, 0)
+
+//console.log(summedPrice)
 
 
 //////////////////PROBLEM 2////////////////////
@@ -44,44 +46,49 @@ const cart = [
     Write a function called `calcFinalPrice` that
     can take in `cartTotal`,`couponValue`,
     and `tax` arguments. 
-
     Inside the function, calculate the tax 
     on the cartTotal and add it in. Subtract
     the value of the coupon. Return the final
     number. 
-
     Note: the numbers passed in for `tax` will be
     decimals, for example: .06 for a 6% tax.
 */
 
 //CODE HERE
 
+const calcFinalPrice = (cartTotal, couponValue, tax) => {
+return (cartTotal * (1+tax)) - couponValue
+}
 
-
+//console.log(calcFinalPrice(27, 5, .06))
+  
 //////////////////PROBLEM 3////////////////////
 /*  
     In this problem, you'll create a model for 
     a customer object as well as an example
     object. 
-
     Plan out a customer object for the cart page.
     Think about the information that a 
     restaurant would need about its customers.
-
     In the TEXT ANSWER area below, describe the
     properties that your customer object will have
     and why you chose those properties.
-
     Explain what data types each property should be
     and why you chose those data types. 
-
     Your object should have at least 4 properties. 
 */
 
 /*
     TEXT ANSWER HERE
 
+name will be a string
+address will be a string
+card information will be a number
+substitutes will be a boolean
+
+
 */
+
 
 /*
     Now, create a customer object following your own
@@ -89,3 +96,10 @@ const cart = [
 */
 
 //CODE HERE
+
+const customer = {
+  name: 'Sumaya',
+  address: '2101 39th 1/2 ave NE',
+  card: 2312312311,
+  substitutes: true
+}
