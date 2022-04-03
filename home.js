cd54d4b7a69240070ab3b00c56e02d5883ad037f
Susaid8 <97367629+Susaid8@users.noreply.github.com>
2022-04-02 21:38:43 -0500

    Update home.js

--- home.js
+++ home.js
@@ -15,7 +15,6 @@
     The function should return a string that says: 
     'Welcome back, [USERNAME]'
     Where USERNAME is the `username` arguement sent in
-
     For example, if called with `Andrew` as the 
     argument, `greetUser` should return the string:
     'Welcome back, Andrew'
@@ -27,7 +26,7 @@ const greetUser = username => {
     return (`Welcome back, ${username}`)
 }
 
-//console.log(greetUser('Andrew'))
+console.log(greetUser('Andrew'))
 
 
 
@@ -37,15 +36,12 @@ const greetUser = username => {
 /* 
     Below is an array of zip codes that are in
     the restaurant's delivery zone. 
-
     Write a function called `canWeDeliver` that
     takes in one argument, `zipCode`.
-
     If the zip code passed in is in the array,
     return a string letting the user know they
     are eligible for delivery. If they are not, 
     return a string letting them know that. 
-
     For example:
     canWeDeliver(84606) 
         // `Sorry, we can't deliver to that address`
@@ -58,7 +54,7 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 //CODE HERE
 
 function canWeDeliver(zipCode) {
-    for(let i = 1; i < deliveryAreaZipCodes.length; i++){
+    for(let i = 0; i < deliveryAreaZipCodes.length; i++){
         if(zipCode === deliveryAreaZipCodes[i]){
             return ('You are in our delivery zone!')
         }else{
@@ -67,19 +63,16 @@ function canWeDeliver(zipCode) {
     }
 }
 
-//console.log(canWeDeliver(55421)) coming back to this
+console.log(canWeDeliver(55421)) 
 
 /* 
     Problem 2 Continued
-
     Now you're going to rewrite your function.
-
     If you wrote `canWeDeliver` using a loop of
     some kind, write a new function (`canWeDeliverTwo`)
     below, using the `includes` array method. 
     Look it up on MDN if you're not sure how to use 
     it. 
-
     If you already used the `includes` method, 
     write a new function using some sort of 
     loop (for loop, higher order array method).
@@ -88,35 +81,44 @@ function canWeDeliver(zipCode) {
 
 // CODE HERE
 
+const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
+
+function canWeDeliverTwo = zipcodes => {
+  if(deliveryAreaZipcodes.includes(zipcodes)){
+    return ('You are in our delivery zone!')
+        }else{
+            return ('Sorry, we can not deliver to that address')
+        }
+    }
+
+console.log(canWeDeliverTwo(55421))
+
 
 //////////////////PROBLEM 3////////////////////
 /* 
     Below is an array of objects that have some
     information about a couple of deals that are
     available at the restaurant currently. 
-
     You are going to access the object's properties 
     and change some values. Don't edit the array 
     directly, let's use the `replace` method.
-
     Read on for more instructions.
 */
 
-const deals = [
-    {
-        title: '15% Off!', 
-        desc: 'Applied to your entire order when you spend $30 or more'
-    }, 
-    {
-        title: 'Free Kids Meal with 2 Regular Entrees', 
-        desc: '   This deal lasts until the end of March! '
-    }
-]
+// const deals = [
+//     {
+//         title: '15% Off!', 
+//         desc: 'Applied to your entire order when you spend $30 or more'
+//     }, 
+//     {
+//         title: 'Free Kids Meal with 2 Regular Entrees', 
+//         desc: '   This deal lasts until the end of March! '
+//     }
+// ]
 
 /*
     The owner has decided to take the 15% off
     deal down to 10%.
-
     Reassign the value of the first deal's title
     to be itself, but use the `replace` method
     to replace the 15 with a 10.
@@ -124,19 +126,27 @@ const deals = [
 
 //CODE HERE
 
+const deals = '15% off'
+
+const dealDown = deals.replace('15', '10')
+console.log(dealDown)
+
 
 
 /*
     The restaurant is going to continue its
     family deal for another month. 
-
     Reassign the value of the second deal's desc
     to be itself, but use the `replace` method
     to replace the word March with April.
-
     You should also make sure that there is no
     whitespace in this string, since it seems
     to be displaying wrong on the live site.
 */
 
 //CODE HERE
+
+const deals = 'Free Kids Meal with 2 Regular Entrees. This deal lasts until the end of March! '
+
+const familyDeal = deals.replace('March', 'April')
+console.log(familyDeal.trim())
